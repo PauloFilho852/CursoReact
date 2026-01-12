672342de5554954a6ba3e33dab30b47adaa8d0ae
PauloFilho <paulofilho055@gmail.com>
2026-01-12 18:02:11 -0300

    Secão 2 - Parte 1

---
+++ 2_FUNDAMENTOS/Fundamentos/src/App.jsx
@@ -0,0 +1,20 @@
+//Componentes
+import FirstComponent from './components/FirstComponente'
+
+//Estilos
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Fundamentos do React</h1>
+      </div>
+      
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
