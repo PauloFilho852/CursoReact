231019efa563323a1353bd8d876f33f009a8a15b
PauloFilho <paulofilho055@gmail.com>
2026-02-22 00:09:25 -0300

    Seção 7 - Parte  2

--- 7_HTTP_REQUEST/http-request/src/App.jsx
+++ 7_HTTP_REQUEST/http-request/src/App.jsx
@@ -1,10 +1,19 @@
-import { use } from 'react'
 import { useState, useEffect } from 'react'
+import './App.css'
 
+//custom hook
+import { useFetch } from './hooks/useFetch'
+
+const url = 'http://localhost:3001/products'
 
 function App() {
   const [products, setProducts] = useState([])
 
+  const { data:items, loading, error, httpConfig } = useFetch(url)
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+/*
   useEffect(() => {
     async function fetchData() {
       try {
@@ -18,16 +27,84 @@ function App() {
 
     fetchData();
   }, [])
+*/
 
-  console.log(products)
+  //Enviar dados para o backend
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+    /*
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, price: parseFloat(price)}),
+      });
+      const newProduct = await response.json();
+      setProducts(prev => [...prev, newProduct]);
+      */
+      
+      httpConfig({ name, price: parseFloat(price)}, 'POST');
+      
+      setName('');
+      setPrice('');
+    }
+  
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
   return (
     <>
-      <div>
-        <ul>
-          {products.map(product => (
-            <li key={product.id}>{product.name} - R$ {product.price}</li>
-          ))}
-        </ul>
+      
+      <div className='center'>        
+        <div >
+          <h1>Lista de Produtos</h1>
+        </div>
+
+        <div >
+          <ul>
+            {items && items.map(product => (
+              <li key={product.id}>{product.name} - R$ {product.price}</li>
+            ))}
+          </ul>
+        </div>
+        
+        <div className='add-product'>
+
+          <h2>Adicionar Produto</h2>
+          <form onSubmit={handleAddProduct}>
+           <label>
+            Nome:
+            <input 
+              type="text"
+              placeholder="Nome do produto"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            </label>
+
+            <label>
+              Preço:
+            <input 
+              type="number"
+              placeholder="Preço do produto"
+              name="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            </label>
+
+            <button type="submit">Adicionar</button>
+          </form>
+
+        </div>
 
       </div>  
     </>
