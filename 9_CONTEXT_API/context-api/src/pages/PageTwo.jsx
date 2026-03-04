d7f91b9a6bc3125c42dbe04e1b4b5251db383a3c
PauloFilho <paulofilho055@gmail.com>
2026-03-03 22:31:11 -0300

    Ajustes

--- 9_CONTEXT_API/context-api/src/pages/PageTwo.jsx
+++ 9_CONTEXT_API/context-api/src/pages/PageTwo.jsx
@@ -2,7 +2,9 @@ import { useCounterContext } from "../hooks/useCounterContext"
 import ChangeCounter from "../components/ChangeCounter"
 
 const PageTwo = () => {
-const {counter} = useCounterContext()
+  //Usando o hook personalizado para acessar o contexto, 
+  //o código fica mais limpo e fácil de ler.
+  const {counter} = useCounterContext()
   return (
     <div>
       <h1>PAGE TWO</h1>
